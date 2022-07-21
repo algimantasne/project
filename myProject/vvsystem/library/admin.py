fb69fc2a246fbf423639bef3ba321f66cac5ef33
Algimantas <98847883+algimantasne@users.noreply.github.com>
2022-07-21 23:39:36 +0300

    Add files via upload

--- myProject/vvsystem/library/admin.py
+++ myProject/vvsystem/library/admin.py
@@ -1,3 +1,16 @@
 from django.contrib import admin
 
 # Register your models here.
+from .models import Supplier, Product, Client, Sale, Expenses
+
+
+class ProductAdmin(admin.ModelAdmin):
+    list_display = ('title', 'supplier', 'price', 'quantity')
+
+
+
+admin.site.register(Supplier)
+admin.site.register(Product, ProductAdmin)
+admin.site.register(Client)
+admin.site.register(Sale)
+admin.site.register(Expenses)
