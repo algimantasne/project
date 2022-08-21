9b02b284b02278a8cb02939a86def8d80cb9106e
Algimantas <98847883+algimantasne@users.noreply.github.com>
2022-08-21 23:12:50 +0300

    Add files via upload
    search

--- myProject/vvsystem/library/urls.py
+++ myProject/vvsystem/library/urls.py
@@ -9,6 +9,7 @@ urlpatterns = [
     path('products/', views.ProductListView.as_view(), name='products'),
     path('products/<int:pk>', views.ProductDetailView.as_view(), name='product-detail'),
     path('search/', views.search, name='search'),
+    path('search/<int:pk>', views.ProductDetailView.as_view(), name='product-detail'),
     path('clients/', views.clients, name='clients'),
     path('clients/<int:client_id>', views.client, name='client'),
     path('managers/', views.managers, name='managers'),
